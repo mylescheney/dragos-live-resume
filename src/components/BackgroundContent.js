5d48a7e9d503c44b0998262b20c5143e2eccf705
Myles <mylescheney@gmail.com>
2022-08-04 09:55:14 -0400

    Maded size adjustments for small and medium screens for introduction sections

--- src/components/BackgroundContent.js
+++ src/components/BackgroundContent.js
@@ -19,10 +19,10 @@ const BackgroundContent = () => {
                 <h2>My Background</h2>
             </div>
             <div className='backgroundBlocksDiv'>
-                <BackgroundBlock content={backgroundPoints[0]} borderColor="#f7ed34" marginLeft={width < 1200 ? '5%' : '0'} />
-                <BackgroundBlock content={backgroundPoints[1]} borderColor="#f73499" marginLeft={width < 1200 ? '5%' : '7%'} />
-                <BackgroundBlock content={backgroundPoints[2]} borderColor="#34f7ac" marginLeft={width < 1200 ? '5%' : '14%'}  />
-                <BackgroundBlock content={backgroundPoints[3]} borderColor="#34e1f7" marginLeft={width < 1200 ? '5%' : '21%'}  />
+                <BackgroundBlock content={backgroundPoints[0]} borderColor="#f7ed34" marginLeft={width < 992 ? '5%' : '0'} />
+                <BackgroundBlock content={backgroundPoints[1]} borderColor="#f73499" marginLeft={width < 992 ? '5%' : '7%'} />
+                <BackgroundBlock content={backgroundPoints[2]} borderColor="#34f7ac" marginLeft={width < 992 ? '5%' : '14%'}  />
+                <BackgroundBlock content={backgroundPoints[3]} borderColor="#34e1f7" marginLeft={width < 992 ? '5%' : '21%'}  />
             </div>
         </div>
     )
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,21 +6,23 @@ const Introduction = () => {
         <div className='introduction'>
             <p className='introMessage'>My name is Miles, but I go by Myles. I'm...</p>
             <div className='skillBlocksDiv'>
-                <div className='skillBlock blockA'>
-                    <h4>Strongest With</h4>
-                    <ul>
-                        <li>React.js</li>
-                        <li>React Native</li>
-                    </ul>
-                </div>
-                <div className='skillBlock blockB'>
-                    <h4>Good With</h4>
-                    <ul>
-                        <li>Express.js</li>
-                        <li>Node.js</li>
-                        <li>MongoDB &#38; Mongoose</li>
-                        <li>Flutter &#38; Dart</li>
-                    </ul>
+                <div className='topSkillBlocksDiv'>
+                    <div className='skillBlock blockA'>
+                        <h4>Strongest With</h4>
+                        <ul>
+                            <li>React.js</li>
+                            <li>React Native</li>
+                        </ul>
+                    </div>
+                    <div className='skillBlock blockB'>
+                        <h4>Good With</h4>
+                        <ul>
+                            <li>Express.js</li>
+                            <li>Node.js</li>
+                            <li>MongoDB &#38; Mongoose</li>
+                            <li>Flutter &#38; Dart</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className='skillBlock blockC'>
                     <h4>Knowledgeable Of</h4>
