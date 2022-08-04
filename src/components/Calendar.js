1b88792ce49274a23fbf78f98435d9763d64fc9c
Myles <mylescheney@gmail.com>
2022-08-04 16:17:06 -0400

    Added weeday labels along the top of the calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -67,6 +67,15 @@ const Calendar = () => {
             <h5>Calendar</h5>
             <div className='sampleBlock calendarBlock'>
                 <h6 className='monthAndYear'>{monthName}, {year}</h6>
+                <div className='weekDaysRow'>
+                    <div className='weekDay'>S</div>
+                    <div className='weekDay'>M</div>
+                    <div className='weekDay'>T</div>
+                    <div className='weekDay'>W</div>
+                    <div className='weekDay'>Th</div>
+                    <div className='weekDay'>F</div>
+                    <div className='weekDay'>S</div>
+                </div>
                 <div className='grid'>
                     <WeekRow rowNum={0} monthLength={monthLength} startDay={startDay} />
                     <WeekRow rowNum={1} monthLength={monthLength} startDay={startDay} />
