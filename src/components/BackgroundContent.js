2e83cf90428ce85d893d4d458573b5b9598d11f5
Myles <mylescheney@gmail.com>
2022-08-03 22:25:34 -0400

    Resized background section for extra small screens

--- src/components/BackgroundContent.js
+++ src/components/BackgroundContent.js
@@ -11,17 +11,18 @@ const BackgroundBlock = ({ content, borderColor, marginLeft }) => {
 }
 
 const BackgroundContent = () => {
-    console.log(backgroundPoints[0]);
+    let width = window.innerWidth;
+    console.log(width);
     return (
         <div className='backgroundContent'>
             <div className='backgroundHeader'>
                 <h2>My Background</h2>
             </div>
             <div className='backgroundBlocksDiv'>
-                <BackgroundBlock content={backgroundPoints[0]} borderColor="#f7ed34" marginLeft='0' />
-                <BackgroundBlock content={backgroundPoints[1]} borderColor="#f73499" marginLeft='7%' />
-                <BackgroundBlock content={backgroundPoints[2]} borderColor="#34f7ac" marginLeft='14%' />
-                <BackgroundBlock content={backgroundPoints[3]} borderColor="#34e1f7" marginLeft='21%' />
+                <BackgroundBlock content={backgroundPoints[0]} borderColor="#f7ed34" marginLeft={width < 1200 ? '5%' : '0'} />
+                <BackgroundBlock content={backgroundPoints[1]} borderColor="#f73499" marginLeft={width < 1200 ? '5%' : '7%'} />
+                <BackgroundBlock content={backgroundPoints[2]} borderColor="#34f7ac" marginLeft={width < 1200 ? '5%' : '14%'}  />
+                <BackgroundBlock content={backgroundPoints[3]} borderColor="#34e1f7" marginLeft={width < 1200 ? '5%' : '21%'}  />
             </div>
         </div>
     )
